01ba7c8ae5aff1d408c95ac440be7fd8506a0483
SerkerkOS <kkauanbastos455@gmail.com>
2026-02-21 21:48:50 -0300

    Add SPA fallback for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 
 function App() {
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const redirect = url.searchParams.get("redirect");
+    if (redirect) {
+      window.history.replaceState(null, "", redirect);
+    }
+  }, []);
+
   return (
     <>
       <Toaster />
